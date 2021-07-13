2c0c5b75a6949f2543e7092b1675e4635eb373fb
Edgar Eldy <ed.eldy21@live.com>
2021-07-13 22:12:36 +0200

    Call navbar-component

--- resources/js/app.js
+++ resources/js/app.js
@@ -33,6 +33,7 @@ const router = new VueRouter({
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('navbar-component', require('./components/partials/NavbarComponent').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
