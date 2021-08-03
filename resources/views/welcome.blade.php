cc16c6ed4699a49b9ab0aa3b88a75c6c503af75c
Edgar Eldy <ed.eldy21@live.com>
2021-08-03 20:02:08 +0200

    Add scripts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,5 +21,44 @@
             </example-component>
         </div>
         <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap-datepicker.js') }}"></script>
+        <script src="{{ asset('js/bootstrap-table.js') }}"></script>
+        <script src="{{ asset('js/fontawesome.min.js') }}"></script>
+
+        <!--Icons-->
+        <script src="{{ asset('js/lumino.glyphs.js') }}"></script>
+        <script>
+            ! function($) {
+                $(document).on("click", "ul.nav li.parent > a > span.icon", function() {
+                    $(this).find('em:first').toggleClass("glyphicon-minus");
+                });
+                $(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
+            }(window.jQuery);
+            $(window).on('resize', function() {
+                if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+            })
+            $(window).on('resize', function() {
+                if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+            })
+
+        </script>
+        <script>
+            $('#calendar').datepicker({});
+            ! function($) {
+                $(document).on("click", "ul.nav li.parent > a > span.icon", function() {
+                    $(this).find('em:first').toggleClass("glyphicon-minus");
+                });
+                $(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
+            }(window.jQuery);
+            $(window).on('resize', function() {
+                if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+            })
+            $(window).on('resize', function() {
+                if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+            })
+
+        </script>
     </body>
 </html>
