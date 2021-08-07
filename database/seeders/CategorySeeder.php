df583016a764534111b5f570a83db05e44ea1ed0
Edgar Eldy <ed.eldy21@live.com>
2021-08-07 22:20:58 +0200

    Populate 5 fake category names

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -13,6 +14,7 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Populate 5 fake category names
+        Category::factory()->count(5)->create();
     }
 }
