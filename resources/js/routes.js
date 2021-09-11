e842705e68b44cd62c2bcb50addadf0d701c00ab
Edgar Eldy <ed.eldy21@live.com>
2021-09-11 19:59:53 +0200

    Add categories route

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,9 +1,13 @@
 // Routes
 import Home from "./components/HomeComponent";
-
+import CategoryIndex from "./components/categories/CategoryIndexComponent";
 export const routes = [
     {
         path: "/",
         component: Home
+    },
+    {
+        path: "/categories",
+        component: CategoryIndex
     }
 ];
