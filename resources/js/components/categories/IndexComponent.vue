e4bca70ad12e183985b938ea00c3a6d006fc4642
Edgar Eldy <ed.eldy21@live.com>
2021-09-11 19:36:53 +0200

    Add categories/IndexComponent for displaying product categories

---
+++ resources/js/components/categories/IndexComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Product categories</div>
+                <div class="panel-body">
+                    <a href="#" class="btn btn-primary">New</a>
+                    <br><br>
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Category name</th>
+                            <th>Options</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>}</td>
+                            <td></td>
+                            <td>
+                                <div class="card-footer">
+                                    <a href="" class="btn btn-primary">Edit</a>
+                                    <form action="#" method="POST">
+                                        <button type="submit"
+                                                onclick="return confirm('Are you sure you want to delete this category ?')"
+                                                class="btn btn-danger btn-sm">Delete
+                                        </button>
+                                    </form>
+
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "IndexComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
