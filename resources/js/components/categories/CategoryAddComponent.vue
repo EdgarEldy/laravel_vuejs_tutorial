bd337abf932340fcdcb307675c7ce9f2dbe899b8
Edgar Eldy <ed.eldy21@live.com>
2021-09-11 21:08:24 +0200

    Design the product category modal

--- resources/js/components/categories/CategoryAddComponent.vue
+++ resources/js/components/categories/CategoryAddComponent.vue
@@ -1,27 +1,32 @@
 <template>
     <div>
         <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">
-            Launch static backdrop modal
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalAddCategory">
+            New
         </button>
 
         <!-- Modal -->
-        <div class="modal fade" id="staticBackdrop" data-backdrop="static" tabindex="-1" role="dialog"
+        <div class="modal fade" id="modalAddCategory" data-backdrop="static" tabindex="-1" role="dialog"
              aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                        <h5 class="modal-title" id="staticBackdropLabel">Product category</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true" class="my-6">&times;</span>
+                            <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
-                        ...
+                        <form action="">
+                            <div class="form-group">
+                                <label for="category name">Category name </label>
+                                <input type="text" class="form-control" value="" />
+                            </div>
+                        </form>
                     </div>
                     <div class="modal-footer">
+                        <button type="button" class="btn btn-primary">Save</button>
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Understood</button>
                     </div>
                 </div>
             </div>
