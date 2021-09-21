3ccdad388ee6519d5ac0ae5d50a8a177f56e9e01
Edgar Eldy <ed.eldy21@live.com>
2021-09-21 20:36:14 +0200

    Change button type to submit

--- resources/js/components/categories/IndexComponent.vue
+++ resources/js/components/categories/IndexComponent.vue
@@ -67,7 +67,7 @@
                             <button class="btn btn-secondary" data-dismiss="modal" type="button">
                                 Close
                             </button>
-                            <button type="button" class="btn btn-primary">Save</button>
+                            <button type="submit" class="btn btn-primary">Save</button>
                         </div>
                     </form>
                 </div>
