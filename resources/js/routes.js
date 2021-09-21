30b7e2baab6865b0e29a2cb8d23f860e6b7df38e
Edgar Eldy <ed.eldy21@live.com>
2021-09-21 18:14:00 +0200

    Change to require a module instead of import it

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,10 +1,9 @@
 // Routes
-import Dashboard from "./components/Dashboard";
 import CategoryIndex from "./components/categories/CategoryIndexComponent";
 export const routes = [
     {
         path: "/",
-        component: Dashboard
+        component: require('./components/Dashboard').default
     },
     {
         path: "/categories",
