746f3c79a3f1fc1404bf7adf19fc2450e6d723e8
Edgar Eldy <ed.eldy21@live.com>
2021-09-21 20:11:54 +0200

    Show Create New Category if editmode is false

--- resources/js/components/categories/IndexComponent.vue
+++ resources/js/components/categories/IndexComponent.vue
@@ -46,6 +46,7 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
+                        <h5 class="modal-title" v-show="!editmode">Create New Category</h5>
                         <button aria-label="Close" class="close" data-dismiss="modal" type="button">
                             <span aria-hidden="true">&times;</span>
                         </button>
