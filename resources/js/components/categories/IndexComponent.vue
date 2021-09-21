32a48cb31577d6acda14c3829804835a9b3cb57f
Edgar Eldy <ed.eldy21@live.com>
2021-09-21 20:10:00 +0200

    Add save button

--- resources/js/components/categories/IndexComponent.vue
+++ resources/js/components/categories/IndexComponent.vue
@@ -61,6 +61,7 @@
                             <button class="btn btn-secondary" data-dismiss="modal" type="button">
                                 Close
                             </button>
+                            <button type="button" class="btn btn-primary">Save</button>
                         </div>
                     </form>
                 </div>
