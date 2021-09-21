6de07e2ee4362b693ab199cc36c20f2c3b255243
Edgar Eldy <ed.eldy21@live.com>
2021-09-21 18:08:42 +0200

    Change to Dashboard component

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,10 +1,10 @@
 // Routes
-import Home from "./components/HomeComponent";
+import Dashboard from "./components/Dashboard";
 import CategoryIndex from "./components/categories/CategoryIndexComponent";
 export const routes = [
     {
         path: "/",
-        component: Home
+        component: Dashboard
     },
     {
         path: "/categories",
