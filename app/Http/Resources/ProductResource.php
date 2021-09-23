15c57c9fee7e7e77adab1dd143765d2623162f83
Edgar Eldy <ed.eldy21@live.com>
2021-09-23 21:08:57 +0200

    Return products response as json

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -14,6 +14,12 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+//        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'category_id' => $this->category_id,
+            'product_name' => $this->product_name,
+            'unit_price' => $this->unit_price
+        ];
     }
 }
