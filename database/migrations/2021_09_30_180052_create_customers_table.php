c8df721362d4f14ccc8b1a6269646f062c89f348
Edgar Eldy <ed.eldy21@live.com>
2021-09-30 20:10:17 +0200

    Update customers migration. Add columns

--- database/migrations/2021_09_30_180052_create_customers_table.php
+++ database/migrations/2021_09_30_180052_create_customers_table.php
@@ -15,6 +15,11 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('tel');
+            $table->string('email');
+            $table->string('address');
             $table->timestamps();
         });
     }
