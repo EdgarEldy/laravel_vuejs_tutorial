e19e25a8615a3d88c5422811ba5f66b96c33b060
Edgar Eldy <ed.eldy21@live.com>
2021-09-30 20:38:34 +0200

    Return customers data as json

--- app/Http/Resources/CustomerResource.php
+++ app/Http/Resources/CustomerResource.php
@@ -14,6 +14,14 @@ class CustomerResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+//        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'tel' => $this->tel,
+            'email' => $this->email,
+            'address' => $this->address
+        ];
     }
 }
