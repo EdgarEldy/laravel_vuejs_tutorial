99c286eee5b4a313ec54645d763055bfd511763a
Edgar Eldy <ed.eldy21@live.com>
2021-10-02 23:22:18 +0200

    Add customer-component

---
+++ resources/js/components/customers/customer-component.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "customer-component",
+    data() {
+        return {}
+    },
+
+    created() {
+
+    },
+
+    // Methods goes here..
+    methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
