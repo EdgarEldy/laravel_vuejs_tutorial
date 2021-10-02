8aba0b62115cbda842877b5db2ec78c5d71282d8
Edgar Eldy <ed.eldy21@live.com>
2021-10-02 23:26:45 +0200

    Design customers table data

--- resources/js/components/customers/customer-component.vue
+++ resources/js/components/customers/customer-component.vue
@@ -1,5 +1,47 @@
 <template>
-
+<div>
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">Customers</div>
+            <div class="panel-body">
+                <!-- Button trigger modal -->
+                <button class="btn btn-primary" type="button">New</button>
+                <br/><br/>
+                <table class="table table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Telephone</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                        <th>Options</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <div class="card-footer">
+                                <a class="btn btn-primary" href="#">Edit</a>
+                                <a class="btn btn-danger" href="#">
+                                    Remove
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
