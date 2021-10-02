a4f19bf06f3c6d06d4509008825648e92b449245
Edgar Eldy <ed.eldy21@live.com>
2021-10-02 23:29:45 +0200

    Load customers from api

--- resources/js/components/customers/customer-component.vue
+++ resources/js/components/customers/customer-component.vue
@@ -1,54 +1,56 @@
 <template>
-<div>
-    <div class="col-lg-12">
-        <div class="panel panel-default">
-            <div class="panel-heading">Customers</div>
-            <div class="panel-body">
-                <!-- Button trigger modal -->
-                <button class="btn btn-primary" type="button">New</button>
-                <br/><br/>
-                <table class="table table-striped table-bordered">
-                    <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>First name</th>
-                        <th>Last name</th>
-                        <th>Telephone</th>
-                        <th>Email</th>
-                        <th>Address</th>
-                        <th>Options</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                            <div class="card-footer">
-                                <a class="btn btn-primary" href="#">Edit</a>
-                                <a class="btn btn-danger" href="#">
-                                    Remove
-                                </a>
-                            </div>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
+    <div>
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Customers</div>
+                <div class="panel-body">
+                    <!-- Button trigger modal -->
+                    <button class="btn btn-primary" type="button">New</button>
+                    <br/><br/>
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Telephone</th>
+                            <th>Email</th>
+                            <th>Address</th>
+                            <th>Options</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                <div class="card-footer">
+                                    <a class="btn btn-primary" href="#">Edit</a>
+                                    <a class="btn btn-danger" href="#">
+                                        Remove
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <script>
 export default {
     name: "customer-component",
     data() {
-        return {}
+        return {
+            customers: {}
+        }
     },
 
     created() {
@@ -56,7 +58,16 @@ export default {
     },
 
     // Methods goes here..
-    methods: {}
+    methods: {
+
+        // Retrieve customers from the backend
+        loadCustomers() {
+            axios
+                .get("/api/customers")
+                .then(response => (this.customers = response.data))
+                .catch(error => console.log(error));
+        },
+    }
 }
 </script>
 
