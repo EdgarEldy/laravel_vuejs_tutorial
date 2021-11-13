e3d8b7db6f19bedc4bcca69662c44a64a52e6494
Edgar Eldy <ed.eldy21@live.com>
2021-11-13 23:00:39 +0200

    Generate 5 fake orders data

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
 use Illuminate\Database\Seeder;
 
 class OrderSeeder extends Seeder
@@ -14,5 +15,6 @@ class OrderSeeder extends Seeder
     public function run()
     {
         //
+        Order::factory(5)->create();
     }
 }
