78108bef1d621bbfd00b234f4b2677b6b705986f
Edgar Eldy <ed.eldy21@live.com>
2021-11-13 23:00:26 +0200

    Add orders fake data

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -2,8 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\Customer;
 use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 class OrderFactory extends Factory
 {
@@ -21,8 +24,15 @@ class OrderFactory extends Factory
      */
     public function definition()
     {
+        $customers= array_column(Customer::all()->toArray(), 'id');
+        $products= array_column(Product::all()->toArray(), 'id');
+
         return [
             //
+            'customer_id' => Arr::random($customers),
+            'product_id' => Arr::random($products),
+            'qty' => $this->faker->randomNumber(2),
+            'total' => $this->faker->randomNumber(4)
         ];
     }
 }
