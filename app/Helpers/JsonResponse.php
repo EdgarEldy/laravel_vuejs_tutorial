5dcf9ffda9bebedc1a8933e37f59c30cec05140f
Edgar Eldy <ed.eldy21@live.com>
2021-12-18 20:24:54 +0200

    Add success response method

--- app/Helpers/JsonResponse.php
+++ app/Helpers/JsonResponse.php
@@ -1,1 +1,20 @@
 <?php
+
+// Success response method
+if (!function_exists('sendResponse')) {
+    /**
+     * @param $result
+     * @param $message
+     * @return mixed
+     */
+    function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message
+        ];
+
+        return $response()->json($response, 200);
+    }
+}
