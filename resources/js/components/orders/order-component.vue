bb29ca916506e64616b479a6a088b81307131074
Edgar Eldy <ed.eldy21@live.com>
2022-01-10 23:33:43 +0200

    Design orders table's data

--- resources/js/components/orders/order-component.vue
+++ resources/js/components/orders/order-component.vue
@@ -1,4 +1,49 @@
 <template>
+    <div>
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Orders</div>
+                <div class="panel-body">
+                    <!-- Button trigger modal -->
+                    <button class="btn btn-primary" type="button">New</button>
+                    <br/><br/>
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Product name</th>
+                            <th>Unit price</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
+                            <th>Options</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                <div class="card-footer">
+                                    <a class="btn btn-primary" href="#">Edit</a>
+                                    <a class="btn btn-danger" href="#">
+                                        Remove
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </template>
 
