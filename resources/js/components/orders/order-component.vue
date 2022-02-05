3d92a64489c36fb330791640ceac25814a93084d
Edgar Eldy <ed.eldy21@live.com>
2022-02-05 20:36:05 +0200

    Add product property

--- resources/js/components/orders/order-component.vue
+++ resources/js/components/orders/order-component.vue
@@ -114,7 +114,8 @@ export default {
             customers: {},
             categories: {},
             products: {},
-            category: ''
+            category: '',
+            product: ''
         }
     },
 
