9b393406c657812d1d84e89101c605d7c2c6b1ae
EdgarEldy <ed.eldy21@live.com>
2022-09-02 12:29:03 +0200

    Set signature command to db:create {name?}

--- app/Console/Commands/dbcreate.php
+++ app/Console/Commands/dbcreate.php
@@ -11,7 +11,7 @@ class dbcreate extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'db:create {name?}';
 
     /**
      * The console command description.
